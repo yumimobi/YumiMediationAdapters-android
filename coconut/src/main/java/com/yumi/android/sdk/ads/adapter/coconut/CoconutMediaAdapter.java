fc9eac537c49a5a3189a8defbbc53d734bf4eebe
MrSongyu <songyujin@zplay.cn>
2017-10-27 18:02:27 +0800

    升级adapter 330，增加椰子和Oneway两家视频Adapter

---
+++ coconut/src/main/java/com/yumi/android/sdk/ads/adapter/coconut/CoconutMediaAdapter.java
@@ -0,0 +1,138 @@
+package com.yumi.android.sdk.ads.adapter.coconut;
+
+import android.app.Activity;
+
+import com.afk.client.ads.ADSDK;
+import com.afk.client.ads.AdEventListener;
+import com.afk.client.ads.VideoStatus;
+import com.yumi.android.sdk.ads.beans.YumiProviderBean;
+import com.yumi.android.sdk.ads.publish.adapter.YumiCustomerMediaAdapter;
+import com.yumi.android.sdk.ads.publish.enumbean.LayerErrorCode;
+import com.yumi.android.sdk.ads.utils.ZplayDebug;
+
+/**
+ * Created by Administrator on 2017/10/27.
+ */
+
+public class CoconutMediaAdapter extends YumiCustomerMediaAdapter{
+    private String TAG = "CoconutMediaAdapter";
+    private Activity activity;
+    private AdEventListener listener;
+    protected CoconutMediaAdapter(Activity activity, YumiProviderBean yumiProviderBean) {
+        super(activity, yumiProviderBean);
+        this.activity = activity;
+    }
+
+    @Override
+    protected void onPrepareMedia() {
+
+    }
+
+    @Override
+    protected void onShowMedia() {
+        if (ADSDK.getInstance().getAdStatus(activity) != VideoStatus.NO_AD) {
+            ADSDK.getInstance().showAdVideo(activity);
+        } else {
+            ADSDK.getInstance().load(activity);
+        }
+    }
+
+    @Override
+    protected boolean isMediaReady() {
+        return false;
+    }
+
+    @Override
+    protected void init() {
+        creatListener();
+        ADSDK.getInstance().setDebug(false).setLogSwitch(false);
+        ADSDK.getInstance().init(activity, getProvider().getKey1(), getProvider().getKey2(),listener);
+
+    }
+
+    private void creatListener() {
+        listener = new AdEventListener() {
+            @Override
+            public void onAdExist(boolean isAdExist, long code) {
+                if (isAdExist) {
+                    layerPrepared();
+                } else {
+                }
+            }
+
+            @Override
+            public void onVideoCached(boolean isCached) {
+                if (isCached) {
+                    ZplayDebug.d(TAG, "椰子已缓存广告视频", onoff);
+                } else {
+                    ZplayDebug.d(TAG, "椰子已缓存失败", onoff);
+                }
+            }
+
+            @Override
+            public void onVideoStart() {
+                ZplayDebug.d(TAG, "Coconut media shown", onoff);
+                layerExposure();
+                layerMediaStart();
+            }
+
+            @Override
+            public void onVideoCompletion(boolean isLookBack) {
+                ZplayDebug.d(TAG, "chartboost media closed", onoff);
+                layerMediaEnd();
+                if (!isLookBack) {
+                    // 可以发放奖励
+
+                }
+            }
+
+            @Override
+            public void onVideoClose(int currentPosition) {
+                layerClosed();
+            }
+
+            @Override
+            public void onVideoError(String error) {
+                if(error.equals(VideoStatus.NO_AD)){
+                    layerPreparedFailed(LayerErrorCode.ERROR_NO_FILL);
+                }else{
+                    layerPreparedFailed(LayerErrorCode.ERROR_INTERNAL);
+                }
+            }
+
+            @Override
+            public void onLandingPageClose(boolean status) {
+                //直接关闭为false，点击下载按钮关闭为true
+                layerClosed();
+                layerIncentived();//依赖三方SDK的光比落地页给出奖励？？？？
+
+            }
+
+            @Override
+            public void onDownloadStart() {
+
+            }
+
+            @Override
+            public void onNetRequestError(String error) {
+                layerPreparedFailed(LayerErrorCode.ERROR_NETWORK_ERROR);
+            }
+        };
+
+    }
+
+    @Override
+    protected void callOnActivityDestroy() {
+        ADSDK.getInstance().release(activity);
+    }
+
+    @Override
+    public void onActivityPause() {
+        ADSDK.getInstance().onPause(activity);
+    }
+
+    @Override
+    public void onActivityResume() {
+        ADSDK.getInstance().onResume(activity);
+    }
+}
---
+++ oneway/src/main/java/com/yumi/android/sdk/ads/adapter/oneway/OnewayMediaAdapter.java
@@ -0,0 +1,103 @@
+package com.yumi.android.sdk.ads.adapter.oneway;
+
+import android.app.Activity;
+
+import com.yumi.android.sdk.ads.beans.YumiProviderBean;
+import com.yumi.android.sdk.ads.publish.adapter.YumiCustomerMediaAdapter;
+import com.yumi.android.sdk.ads.publish.enumbean.LayerErrorCode;
+import com.yumi.android.sdk.ads.utils.ZplayDebug;
+
+import mobi.oneway.sdk.OnewaySdk;
+import mobi.oneway.sdk.OnewaySdkError;
+import mobi.oneway.sdk.OnewaySdkListener;
+import mobi.oneway.sdk.OnewayVideoFinishType;
+
+/**
+ * Created by Administrator on 2017/10/27.
+ */
+
+public class OnewayMediaAdapter extends YumiCustomerMediaAdapter {
+    private String TAG = "OnewayMediaAdapter";
+    private Activity activity;
+    private OnewaySdkListener listener;
+    protected OnewayMediaAdapter(Activity activity, YumiProviderBean yumiProviderBean) {
+        super(activity, yumiProviderBean);
+        this.activity = activity;
+    }
+
+    @Override
+    protected void onPrepareMedia() {
+
+    }
+
+    @Override
+    protected void onShowMedia() {
+        OnewaySdk.showAdVideo(activity);
+    }
+
+    @Override
+    protected boolean isMediaReady() {
+        return false;
+    }
+
+    @Override
+    protected void init() {
+        creatListener();
+        OnewaySdk.init(activity,getProvider().getKey1(),listener);
+    }
+
+    private void creatListener() {
+        listener = new OnewaySdkListener() {
+            @Override
+            public void onAdReady(String placementID) {
+                ZplayDebug.d(TAG, "Oneway media prepared", onoff);
+                layerPrepared();
+            }
+
+            @Override
+            public void onAdStart(String placementID) {
+                ZplayDebug.d(TAG, "Oneway media shown", onoff);
+                layerExposure();
+                layerMediaStart();
+            }
+
+            @Override
+            public void onAdFinish(String placementID, OnewayVideoFinishType onewayVideoFinishType) {
+                ZplayDebug.d(TAG, "chartboost media closed", onoff);
+                layerMediaEnd();
+                layerClosed();
+                layerIncentived();
+            }
+
+            @Override
+            public void onSdkError(OnewaySdkError onewaySdkError, String s) {
+                layerPreparedFailed(decodeError(onewaySdkError));
+            }
+        };
+    }
+
+    private LayerErrorCode decodeError(OnewaySdkError onewaySdkError) {
+        if(onewaySdkError.equals(OnewaySdkError.CAMPAIGN_NO_FILL)){
+            return LayerErrorCode.ERROR_NO_FILL;
+        }
+        if (onewaySdkError.equals(OnewaySdkError.INITIALIZE_FAILED)) {
+            return LayerErrorCode.ERROR_NETWORK_ERROR;
+        }
+        return LayerErrorCode.ERROR_INTERNAL;
+    }
+
+    @Override
+    protected void callOnActivityDestroy() {
+
+    }
+
+    @Override
+    public void onActivityPause() {
+
+    }
+
+    @Override
+    public void onActivityResume() {
+
+    }
+}
