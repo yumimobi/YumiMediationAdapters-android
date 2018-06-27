3be6e8fe7e9d941a4ce916ba007df507e51eb44e
HeJiLong <he_jilong@126.com>
2018-06-27 17:30:26 +0800

    applovin SDK 升级到 8.0.2  使用ZoneID 版本

--- applovin/src/main/java/com/yumi/android/sdk/ads/adapter/applovin/ApplovinExtraHolder.java
+++ applovin/src/main/java/com/yumi/android/sdk/ads/adapter/applovin/ApplovinExtraHolder.java
@@ -6,32 +6,23 @@ import com.applovin.sdk.AppLovinSdk;
 import com.applovin.sdk.AppLovinSdkSettings;
 import com.yumi.android.sdk.ads.publish.YumiDebug;
 
+/**
+ * Created by hjl on 2018/6/21.
+ */
 public class ApplovinExtraHolder {
+    private static AppLovinSdk sdk = null;
 
-	private static boolean hasInit = false;
-	private static AppLovinSdk sdk = null;
-	
-	
-	static void initApplovinSDK(Activity activity, String appkey){
-		if (!hasInit ) {
-			AppLovinSdkSettings settings = new AppLovinSdkSettings();
-			settings.setVerboseLogging(YumiDebug.isDebugMode());
-			settings.setAutoPreloadTypes("NONE");
-//			settings.setAutoPreloadTypes("VIDEOA,REGULAR");
-			sdk = AppLovinSdk.getInstance(appkey, settings, activity);
-			hasInit = true;
-		}
-	}
+    public static AppLovinSdk getAppLovinSDK(Activity activity, String appkey) {
+        if (sdk == null) {
+            AppLovinSdkSettings settings = new AppLovinSdkSettings();
+            settings.setVerboseLogging(YumiDebug.isDebugMode());
+//            settings.setBannerAdRefreshSeconds(15);  设置banner刷新秒数
+            sdk = AppLovinSdk.getInstance(appkey, settings, activity);
+        }
+        return sdk;
+    }
 
-	static AppLovinSdk getAppLovinSDK(){
-		return sdk;
-	}
-
-	
-	static void destroyHolder(){
-		hasInit = false;
-		sdk = null;
-	}
-	
-	
+    public static void destroyHolder() {
+        sdk = null;
+    }
 }
