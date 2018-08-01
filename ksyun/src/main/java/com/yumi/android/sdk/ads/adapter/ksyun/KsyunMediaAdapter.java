54f806b80e5baa1c07f7040a999e7af2c9f96c29
HeJiLong <he_jilong@126.com>
2018-08-01 15:04:17 +0800

    增加金山云适配器

---
+++ ksyun/src/main/java/com/yumi/android/sdk/ads/adapter/ksyun/KsyunMediaAdapter.java
@@ -0,0 +1,172 @@
+package com.yumi.android.sdk.ads.adapter.ksyun;
+
+import android.app.Activity;
+
+import com.ksc.ad.sdk.IKsyunAdInitResultListener;
+import com.ksc.ad.sdk.IKsyunAdListener;
+import com.ksc.ad.sdk.IKsyunAdLoadListener;
+import com.ksc.ad.sdk.IKsyunRewardVideoAdListener;
+import com.ksc.ad.sdk.KsyunAdSdk;
+import com.ksc.ad.sdk.KsyunAdSdkConfig;
+import com.yumi.android.sdk.ads.beans.YumiProviderBean;
+import com.yumi.android.sdk.ads.publish.adapter.YumiCustomerMediaAdapter;
+import com.yumi.android.sdk.ads.publish.enumbean.LayerErrorCode;
+import com.yumi.android.sdk.ads.utils.ZplayDebug;
+
+import java.util.Map;
+
+/**
+ * Created by hjl on 2018/7/30.
+ */
+public class KsyunMediaAdapter extends YumiCustomerMediaAdapter {
+
+    private static final String TAG = "KsyunMediaAdapter";
+    private IKsyunAdListener adListener;
+    private IKsyunRewardVideoAdListener rewardVideoAdListener;
+
+    protected KsyunMediaAdapter(Activity activity, YumiProviderBean provider) {
+        super(activity, provider);
+    }
+
+    @Override
+    public void onActivityPause() {
+
+    }
+
+    @Override
+    public void onActivityResume() {
+
+    }
+
+    @Override
+    protected void onPrepareMedia() {
+        ZplayDebug.d(TAG, "Ksyun request new media", onoff);
+        loadAd();
+    }
+
+    @Override
+    protected void onShowMedia() {
+        ZplayDebug.i(TAG, "Ksyun Media onShowMedia : " + getProvider().getKey2(), onoff);
+        if (KsyunAdSdk.getInstance().hasAd(getProvider().getKey2())) {
+            //广告存在，点击奖励视频入口后，调用showAd接口展示广告
+            KsyunAdSdk.getInstance().showAd(getActivity(), getProvider().getKey2());
+        } else {
+            loadAd();
+        }
+    }
+
+    @Override
+    protected boolean isMediaReady() {
+        boolean isReady = KsyunAdSdk.getInstance().hasAd(getProvider().getKey2());
+        ZplayDebug.i(TAG, "Ksyun Media isMediaReady : " + isReady, onoff);
+        return isReady;
+    }
+
+    @Override
+    protected void init() {
+        ZplayDebug.i(TAG, "Ksyun Media init Key1 : " + getProvider().getKey1() + "  Key2 : " + getProvider().getKey2(), onoff);
+        createMediaListener();
+        //设置SDK请求环境为线上环境。SDK的init初始化方法，如果不设置config，默认则为沙盒环境
+        KsyunAdSdkConfig config = new KsyunAdSdkConfig();
+        config.setSdkEnvironment(KsyunAdSdkConfig.RELEASE_ENV);
+        //设置奖励视频展示过程中，允许出现关闭按钮
+        config.setShowCloseBtnOfRewardVideo(false);
+        //设置奖励视频展示过程中，出现关闭按钮的时间点
+//        config.setCloseBtnComingTimeOfRewardVideo(5);
+        KsyunAdSdk.getInstance().init(getActivity(), getProvider().getKey1(), config, new IKsyunAdInitResultListener() {
+            @Override
+            public void onSuccess(Map<String, String> map) {
+                ZplayDebug.i(TAG, "Ksyun SDK init onSuccess : ", onoff);
+                //SDK初始化成功，设置事件监听
+                KsyunAdSdk.getInstance().setAdListener(adListener);
+                KsyunAdSdk.getInstance().setRewardVideoAdListener(rewardVideoAdListener);
+            }
+
+            @Override
+            public void onFailure(int errCode, String errMsg) {
+                ZplayDebug.i(TAG, "Ksyun SDK init onFailure errCode: " + errCode + "  errMsg:" + errMsg, onoff);
+            }
+        });
+    }
+
+    private void loadAd() {
+        KsyunAdSdk.getInstance().loadAd(new IKsyunAdLoadListener() {
+            @Override
+            public void onAdInfoSuccess() {
+                ZplayDebug.i(TAG, "Ksyun Media onAdInfoSuccess", onoff);
+            }
+
+            @Override
+            public void onAdInfoFailed(int erroCode, String erroMsg) {
+                ZplayDebug.i(TAG, "Ksyun Media onShowSuccess  erroCode: " + erroCode + "   erroMsg: " + erroMsg, onoff);
+                if (erroCode == 2001) {
+                    layerPreparedFailed(LayerErrorCode.ERROR_NO_FILL);
+                } else {
+                    layerPreparedFailed(LayerErrorCode.ERROR_INTERNAL);
+                }
+            }
+
+            @Override
+            public void onAdLoaded(String adSlotId) {
+                ZplayDebug.i(TAG, "Ksyun Media onAdLoaded  adSlotId: " + adSlotId, onoff);
+                layerPrepared();
+            }
+        });
+    }
+
+    private void createMediaListener() {
+        adListener = new IKsyunAdListener() {
+            @Override
+            public void onShowSuccess(String adSlotId) {
+                ZplayDebug.i(TAG, "Ksyun Media onShowSuccess  adSlotId: " + adSlotId, onoff);
+                layerExposure();
+                layerMediaStart();
+            }
+
+            @Override
+            public void onShowFailed(String adSlotId, int erroCode, String erroMsg) {
+                //播放失败,预加载下一个奖励视频
+                ZplayDebug.i(TAG, "Ksyun Media onShowFailed  adSlotId: " + adSlotId + "  erroCode : " + erroCode + "   erroMsg : " + erroMsg, onoff);
+            }
+
+            @Override
+            public void onADComplete(String adSlotId) {
+                //播放成功，预加载下一个奖励视频
+                ZplayDebug.i(TAG, "Ksyun Media onADComplete  adSlotId: " + adSlotId, onoff);
+                layerMediaEnd();
+            }
+
+            @Override
+            public void onADClick(String adSlotId) {
+                ZplayDebug.i(TAG, "Ksyun Media onADClick  adSlotId: " + adSlotId, onoff);
+                layerClicked();
+            }
+
+            @Override
+            public void onADClose(String adSlotId) {
+                ZplayDebug.i(TAG, "Ksyun Media onADClose  adSlotId: " + adSlotId, onoff);
+                layerClosed();
+            }
+        };
+
+        rewardVideoAdListener = new IKsyunRewardVideoAdListener() {
+            @Override
+            public void onAdAwardSuccess(String s) {
+                ZplayDebug.i(TAG, "Ksyun Media onAdAwardSuccess  s: " + s, onoff);
+                layerIncentived();
+            }
+
+            @Override
+            public void onAdAwardFailed(String s, int i, String s1) {
+                ZplayDebug.i(TAG, "Ksyun Media onAdAwardFailed  s: " + s + "  i : " + i + "   s1 : " + s1, onoff);
+
+            }
+        };
+    }
+
+
+    @Override
+    protected void callOnActivityDestroy() {
+    }
+
+}
