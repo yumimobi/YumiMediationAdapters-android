df67ae99b06e46ac5c5fac2142663c343ee45dfe
zmer007 <developer.lgd@gmail.com>
2019-01-22 14:48:26 +0800

    update inmobi errorMsg

--- inmobi/src/main/java/com/yumi/android/sdk/ads/adapter/inmobi/InmobiExtraHolder.java
+++ inmobi/src/main/java/com/yumi/android/sdk/ads/adapter/inmobi/InmobiExtraHolder.java
@@ -2,6 +2,7 @@ package com.yumi.android.sdk.ads.adapter.inmobi;
 
 import android.app.Activity;
 
+import com.inmobi.ads.InMobiAdRequestStatus;
 import com.inmobi.ads.InMobiAdRequestStatus.StatusCode;
 import com.inmobi.sdk.InMobiSdk;
 import com.yumi.android.sdk.ads.publish.enumbean.LayerErrorCode;
@@ -18,28 +19,34 @@ public class InmobiExtraHolder {
 		}
 	}
 
-    public static LayerErrorCode decodeError(StatusCode code) {
-        LayerErrorCode error;
-        if(code == null){
+    public static LayerErrorCode decodeError(InMobiAdRequestStatus inMobiAdRequestStatus) {
+	    if (inMobiAdRequestStatus == null) {
             return LayerErrorCode.ERROR_INTERNAL;
         }
-
+        StatusCode code = inMobiAdRequestStatus.getStatusCode();
+	    if(code == null){
+	        LayerErrorCode result = LayerErrorCode.ERROR_INTERNAL;
+	        result.setExtraMsg(inMobiAdRequestStatus.getMessage());
+	        return result;
+        }
+        
+        LayerErrorCode result;
         switch (code) {
             case NO_FILL:
-                error = LayerErrorCode.ERROR_NO_FILL;
+                result = LayerErrorCode.ERROR_NO_FILL;
                 break;
             case REQUEST_INVALID:
-                error = LayerErrorCode.ERROR_INVALID;
+                result = LayerErrorCode.ERROR_INVALID;
                 break;
             case NETWORK_UNREACHABLE:
-                error = LayerErrorCode.ERROR_NETWORK_ERROR;
+                result = LayerErrorCode.ERROR_NETWORK_ERROR;
                 break;
             default:
-                error = LayerErrorCode.ERROR_INTERNAL;
+                result = LayerErrorCode.ERROR_INTERNAL;
                 break;
         }
-        error.setExtraMsg(code.toString());
-        return error;
+        result.setExtraMsg("Inmobi errorMsg: " + inMobiAdRequestStatus.getMessage());
+        return result;
     }
 
 
