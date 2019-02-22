a3a98db35712e5209b57a427e708f817e30d6d1f
zmer007 <developer.lgd@gmail.com>
2019-02-22 14:18:21 +0800

    IQZone: smart banner request will results failed response directly

--- iqzone/src/main/java/com/yumi/android/sdk/ads/adapter/iqzone/IQZoneUtil.java
+++ iqzone/src/main/java/com/yumi/android/sdk/ads/adapter/iqzone/IQZoneUtil.java
@@ -2,14 +2,26 @@ package com.yumi.android.sdk.ads.adapter.iqzone;
 
 import com.yumi.android.sdk.ads.publish.enumbean.LayerErrorCode;
 
+import static android.text.TextUtils.isEmpty;
+
 /**
  * Description:
  * <p>
  * Created by lgd on 2019/1/28.
  */
-public class IQZoneUtil {
-    public static LayerErrorCode recodeError(LayerErrorCode layerErrorCode){
-        layerErrorCode.setExtraMsg("IQZone errorMsg: null");
+class IQZoneUtil {
+    static LayerErrorCode recodeError(LayerErrorCode layerErrorCode) {
+        return recodeError(layerErrorCode, null);
+    }
+
+    static LayerErrorCode recodeError(LayerErrorCode layerErrorCode, String yumiLog) {
+        String extraMsg = "IQZone errorMsg: null";
+
+        if (!isEmpty(yumiLog)) {
+            extraMsg += ", " + yumiLog;
+        }
+
+        layerErrorCode.setExtraMsg(extraMsg);
         return layerErrorCode;
     }
 }
