282e6622ff66282e5a7ab7e9f93522a39b395843
“yangfengbang” <yangfengbang@zplay.cn>
2019-04-25 11:23:15 +0800

    media closed callback add isCompletePlaying code

--- unity-china/src/main/java/com/yumi/android/sdk/ads/adapter/unity/UnityMediaAdapter.java
+++ unity-china/src/main/java/com/yumi/android/sdk/ads/adapter/unity/UnityMediaAdapter.java
@@ -70,11 +70,13 @@ public class UnityMediaAdapter extends YumiCustomerMediaAdapter {
 					public void onUnityAdsFinish(String zoneId, FinishState result) {
 						ZplayDebug.d(TAG, "unity media onUnityAdsFinish zoneId : " + zoneId + "  FinishState : " + result, onoff);
 						if (getProvider().getKey2().equals(zoneId)) {
+							boolean isCompletePlaying = false;
 							if (result == FinishState.COMPLETED) {
+								isCompletePlaying = true;
 								layerIncentived();
 								ZplayDebug.d(TAG, "unity media onUnityAdsFinish layerIncentived ", onoff);
 							}
-							layerClosed();
+							layerClosed(isCompletePlaying);
 							ZplayDebug.d(TAG, "unity media onUnityAdsFinish layerClosed layerMediaEnd ", onoff);
 						}
 					}
