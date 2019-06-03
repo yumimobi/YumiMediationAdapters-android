a8cf5c1c4eb257f65bb5482df499dec52b4890b6
zmer007 <developer.lgd@gmail.com>
2019-06-03 17:29:35 +0800

    add gdpr settings to iqzone sdk

--- iqzone/src/main/java/com/yumi/android/sdk/ads/adapter/iqzone/IQZoneInterstitialAdapter.java
+++ iqzone/src/main/java/com/yumi/android/sdk/ads/adapter/iqzone/IQZoneInterstitialAdapter.java
@@ -9,6 +9,7 @@ import com.yumi.android.sdk.ads.publish.adapter.YumiCustomerInterstitialAdapter;
 import com.yumi.android.sdk.ads.utils.ZplayDebug;
 
 import static com.yumi.android.sdk.ads.adapter.iqzone.IQZoneUtil.recodeError;
+import static com.yumi.android.sdk.ads.adapter.iqzone.IQZoneUtil.updateGDPRStatus;
 import static com.yumi.android.sdk.ads.publish.enumbean.LayerErrorCode.CODE_FAILED;
 
 /**
@@ -23,6 +24,7 @@ public class IQZoneInterstitialAdapter extends YumiCustomerInterstitialAdapter {
 
     protected IQZoneInterstitialAdapter(Activity activity, YumiProviderBean yumiProviderBean) {
         super(activity, yumiProviderBean);
+        updateGDPRStatus(activity);
     }
 
     @Override
