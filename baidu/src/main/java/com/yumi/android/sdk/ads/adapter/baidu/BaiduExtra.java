7969dcae25ab921c2e131890435a15a7cfd9aba8
“yangfengbang” <yangfengbang@zplay.cn>
2019-06-14 18:19:07 +0800

    fix. update baidu sdk 3.6.3.0，baidu sdk Interstitial ad load error

---
+++ baidu/src/main/java/com/yumi/android/sdk/ads/adapter/baidu/BaiduExtra.java
@@ -0,0 +1,34 @@
+package com.yumi.android.sdk.ads.adapter.baidu;
+
+
+import android.app.Activity;
+
+import com.baidu.mobads.InterstitialAd;
+import com.baidu.mobads.InterstitialAdListener;
+
+public class BaiduExtra {
+
+    private InterstitialAd instertitial;
+
+    private BaiduExtra() {
+    }
+
+    static BaiduExtra getBaiduExtra() {
+        return BaiduExtraHolder.INSTANCE;
+    }
+
+    private static class BaiduExtraHolder {
+
+        private static final BaiduExtra INSTANCE = new BaiduExtra();
+
+    }
+
+    public InterstitialAd getBaiduInterstitialAd(Activity activity, String key2, InterstitialAdListener instertitialListener) {
+
+        if (instertitial == null) {
+            instertitial = new InterstitialAd(activity, key2);
+        }
+        instertitial.setListener(instertitialListener);
+        return instertitial;
+    }
+}
