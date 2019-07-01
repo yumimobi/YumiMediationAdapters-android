deae621938ad5207a2971a4fcf60e730d33c0017
zmer007 <developer.lgd@gmail.com>
2019-07-01 17:06:29 +0800

    fine tune isReady and comments

--- tapjoy/src/main/java/com/yumi/android/sdk/ads/adapter/tapjoy/TapjoyHelper.java
+++ tapjoy/src/main/java/com/yumi/android/sdk/ads/adapter/tapjoy/TapjoyHelper.java
@@ -8,6 +8,7 @@ import com.tapjoy.Tapjoy;
 import com.tapjoy.TapjoyConnectFlag;
 import com.yumi.android.sdk.ads.publish.AdError;
 import com.yumi.android.sdk.ads.publish.enumbean.LayerErrorCode;
+import com.yumi.android.sdk.ads.utils.ZplayDebug;
 
 import java.util.Hashtable;
 
@@ -16,7 +17,9 @@ import java.util.Hashtable;
  * <p>
  * Created by lgd on 2019-06-25.
  */
-public class TapjoyHelper {
+class TapjoyHelper {
+    private static final String TAG = "TapjoyHelper";
+
     static void connectTapjoy(Context context, String tapjoySDKKey, final TJConnectListener tjConnectListener) {
         Hashtable<String, Object> connectFlags = new Hashtable<>();
         connectFlags.put(TapjoyConnectFlag.ENABLE_LOGGING, "true");
@@ -35,6 +38,10 @@ public class TapjoyHelper {
     }
 
     static AdError recodeError(TJError tjError) {
+        // 未找到相关错误码说明
+        // 接入文档：https://dev.tapjoy.com/zh/sdk-integration/android/getting-started-guide-publishers-android/
+        // API 文档：https://ltv.tapjoy.com/sdk/api/java/index.html
+        ZplayDebug.d(TAG, "recodeError: " + tjError.code + ", msg: " + tjError.message);
         return new AdError(LayerErrorCode.ERROR_NO_FILL);
     }
 }
