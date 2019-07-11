bccc6d1f282eab1f88d8af69ffa93e40cdd02334
zmer007 <developer.lgd@gmail.com>
2019-07-11 18:38:32 +0800

    add unity clicked callback

--- unity/src/main/java/com/yumi/android/sdk/ads/adapter/unity/UnityAdsListenerObserver.java
+++ unity/src/main/java/com/yumi/android/sdk/ads/adapter/unity/UnityAdsListenerObserver.java
@@ -1,7 +1,7 @@
 package com.yumi.android.sdk.ads.adapter.unity;
 
-import com.unity3d.ads.IUnityAdsListener;
 import com.unity3d.ads.UnityAds;
+import com.unity3d.ads.mediation.IUnityAdsExtendedListener;
 
 import java.lang.ref.WeakReference;
 
@@ -9,19 +9,19 @@ import static com.yumi.android.sdk.ads.adapter.unity.UnityAdsListenerObserver.Un
 import static com.yumi.android.sdk.ads.adapter.unity.UnityAdsListenerObserver.UnityAdStatus.IDLE;
 import static com.yumi.android.sdk.ads.adapter.unity.UnityAdsListenerObserver.UnityAdStatus.READY;
 
-class UnityAdsListenerObserver implements IUnityAdsListener {
+class UnityAdsListenerObserver implements IUnityAdsExtendedListener {
     private UnityAdStatus mUnityAdStatus = IDLE;
     private String mPlacementId;
     private UnityAds.UnityAdsError mAdsError;
 
-    private WeakReference<IUnityAdsListener> mObserverRef = new WeakReference<>(null);
+    private WeakReference<IUnityAdsExtendedListener> mObserverRef = new WeakReference<>(null);
 
     @Override
     public void onUnityAdsReady(String placementId) {
         mPlacementId = placementId;
         mUnityAdStatus = READY;
 
-        IUnityAdsListener observer = mObserverRef.get();
+        IUnityAdsExtendedListener observer = mObserverRef.get();
         if (observer != null) {
             observer.onUnityAdsReady(placementId);
         }
@@ -31,7 +31,7 @@ class UnityAdsListenerObserver implements IUnityAdsListener {
     public void onUnityAdsStart(String placementId) {
         mPlacementId = placementId;
 
-        IUnityAdsListener observer = mObserverRef.get();
+        IUnityAdsExtendedListener observer = mObserverRef.get();
         if (observer != null) {
             observer.onUnityAdsStart(placementId);
         }
@@ -42,7 +42,7 @@ class UnityAdsListenerObserver implements IUnityAdsListener {
         mPlacementId = placementId;
         mUnityAdStatus = IDLE;
 
-        IUnityAdsListener observer = mObserverRef.get();
+        IUnityAdsExtendedListener observer = mObserverRef.get();
         if (observer != null) {
             observer.onUnityAdsFinish(placementId, finishState);
         }
@@ -54,13 +54,29 @@ class UnityAdsListenerObserver implements IUnityAdsListener {
         mPlacementId = placementId;
         mUnityAdStatus = ERROR;
 
-        IUnityAdsListener observer = mObserverRef.get();
+        IUnityAdsExtendedListener observer = mObserverRef.get();
         if (observer != null) {
             observer.onUnityAdsError(unityAdsError, placementId);
         }
     }
 
-    void setObserver(IUnityAdsListener listener) {
+
+    @Override
+    public void onUnityAdsClick(String placementId) {
+        mPlacementId = placementId;
+
+        IUnityAdsExtendedListener observer = mObserverRef.get();
+        if (observer != null) {
+            observer.onUnityAdsClick(placementId);
+        }
+    }
+
+    @Override
+    public void onUnityAdsPlacementStateChanged(String placementId, UnityAds.PlacementState placementState, UnityAds.PlacementState placementState1) {
+
+    }
+
+    void setObserver(IUnityAdsExtendedListener listener) {
         mObserverRef = new WeakReference<>(listener);
         switch (mUnityAdStatus) {
             case READY:
