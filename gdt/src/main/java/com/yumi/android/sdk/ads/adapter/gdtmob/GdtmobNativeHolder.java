52d266d61e2f4528620e3e07156fb117a7d51e7f
“yangfengbang” <yangfengbang@zplay.cn>
2019-07-12 11:35:27 +0800

    gdt native ad mediation templateView type

---
+++ gdt/src/main/java/com/yumi/android/sdk/ads/adapter/gdtmob/GdtmobNativeHolder.java
@@ -0,0 +1,55 @@
+package com.yumi.android.sdk.ads.adapter.gdtmob;
+
+import android.content.Context;
+
+import com.qq.e.ads.cfg.VideoOption;
+import com.qq.e.ads.nativ.ADSize;
+import com.qq.e.ads.nativ.NativeADUnifiedListener;
+import com.qq.e.ads.nativ.NativeExpressAD;
+import com.qq.e.ads.nativ.NativeExpressAD.NativeExpressADListener;
+import com.qq.e.ads.nativ.NativeUnifiedAD;
+import com.yumi.android.sdk.ads.utils.ZplayDebug;
+
+public class GdtmobNativeHolder {
+    private String TAG = "GdtmobNativeHolder";
+    private boolean onoff = true;
+    private static GdtmobNativeHolder instance;
+    private NativeUnifiedAD nativeAD;
+
+    private NativeExpressAD nativeExpressAD;
+
+    public static GdtmobNativeHolder getInstance() {
+        if (instance == null) {
+            instance = new GdtmobNativeHolder();
+        }
+        return instance;
+    }
+
+
+    public void initNativeUnifiedAD(Context mContext, String key1, String key2, NativeADUnifiedListener unifiedListener) {
+        nativeAD = new NativeUnifiedAD(mContext, key1, key2, unifiedListener);
+    }
+
+    public void loadNativeUnifiedAD(int currentPoolSpace){
+        if (nativeAD != null) {
+            ZplayDebug.v(TAG, "Gdt nativeUnifiedAD onPrepareNative adCount=" + currentPoolSpace, onoff);
+            nativeAD.loadData(currentPoolSpace);
+        }
+    }
+
+    public void initNativeExpressAD(Context mContext, String key1, String key2, NativeExpressADListener expressADListeners ){
+        nativeExpressAD = new NativeExpressAD(mContext, new ADSize(ADSize.FULL_WIDTH, ADSize.AUTO_HEIGHT), key1, key2, expressADListeners); // 传入Activity
+        // 注意：如果您在联盟平台上新建原生模板广告位时，选择了支持视频，那么可以进行个性化设置（可选）
+        nativeExpressAD.setVideoOption(new VideoOption.Builder()
+                .setAutoPlayPolicy(VideoOption.AutoPlayPolicy.WIFI) // WIFI 环境下可以自动播放视频
+                .setAutoPlayMuted(true) // 自动播放时为静音
+                .build());
+    }
+
+    public void loadNativeExpressAD(int currentPoolSpace){
+        if (nativeExpressAD != null) {
+            ZplayDebug.v(TAG, "Gdt nativeExpressAD onPrepareNative adCount=" + currentPoolSpace, onoff);
+            nativeExpressAD.loadAD(currentPoolSpace);
+        }
+    }
+}
