f0661e2ba2f8a2ab016d32ba131d5e9ea909337b
zmer007 <developer.lgd@gmail.com>
2019-07-26 10:59:05 +0800

    fix, cannot load baidu banner

--- baidu/src/main/java/com/yumi/android/sdk/ads/adapter/baidu/BaiduUtil.java
+++ baidu/src/main/java/com/yumi/android/sdk/ads/adapter/baidu/BaiduUtil.java
@@ -1,5 +1,9 @@
 package com.yumi.android.sdk.ads.adapter.baidu;
 
+import android.content.res.Resources;
+import android.os.Build;
+import android.util.DisplayMetrics;
+
 import com.baidu.mobad.feeds.NativeErrorCode;
 import com.yumi.android.sdk.ads.publish.AdError;
 import com.yumi.android.sdk.ads.publish.enumbean.LayerErrorCode;
@@ -30,5 +34,29 @@ class BaiduUtil {
         AdError result = new AdError(errCode);
         result.setErrorMessage("Baidu errorMsg: " + errMsg);
         return result;
+    }
+
+    static int dp2px(float dp) {
+        float density = Resources.getSystem().getDisplayMetrics().density;
+        return ((int) (dp * density + 0.5f));
+    }
+
+    static boolean isTablet() {
+        if (Build.VERSION.SDK_INT >= 17) {
+            DisplayMetrics displayMetrics = Resources.getSystem().getDisplayMetrics();
+            float density = displayMetrics.density;
+            double inch = Math.sqrt(Math.pow(displayMetrics.widthPixels, 2) + Math.pow(displayMetrics.heightPixels, 2)) / (160 * density);
+            return inch >= 8.0d;
+        }
+        return isApproximateTablet();
+    }
+
+    private static boolean isApproximateTablet() {
+        DisplayMetrics displayMetrics = Resources.getSystem().getDisplayMetrics();
+        int width = displayMetrics.widthPixels;
+        int height = displayMetrics.heightPixels;
+        float density = displayMetrics.density;
+        double inch = Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2)) / (160 * density);
+        return inch >= 8.0d;
     }
 }
