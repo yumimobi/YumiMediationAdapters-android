ac9683f34e4c8b9db589c258226c3e883b7f917e
“yangfengbang” <yangfengbang@zplay.cn>
2019-08-14 10:32:36 +0800

    add pubnative platform

---
+++ pubnative/src/main/java/com/yumi/android/sdk/ads/adapter/pubnative/PubNativeUtil.java
@@ -0,0 +1,70 @@
+package com.yumi.android.sdk.ads.adapter.pubnative;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Build;
+import android.util.DisplayMetrics;
+
+import com.yumi.android.sdk.ads.publish.AdError;
+import com.yumi.android.sdk.ads.publish.YumiDebug;
+import com.yumi.android.sdk.ads.publish.YumiSettings;
+import com.yumi.android.sdk.ads.publish.enumbean.LayerErrorCode;
+import com.yumi.android.sdk.ads.publish.enumbean.YumiGDPRStatus;
+
+import net.pubnative.lite.sdk.PNLite;
+
+public class PubNativeUtil {
+
+
+    static AdError recodeError(String errMsg) {
+        LayerErrorCode errCode = LayerErrorCode.ERROR_NO_FILL;
+        AdError result = new AdError(errCode);
+        result.setErrorMessage("Baidu errorMsg: " + errMsg);
+        return result;
+    }
+
+    static int dp2px(float dp) {
+        float density = Resources.getSystem().getDisplayMetrics().density;
+        return ((int) (dp * density + 0.5f));
+    }
+
+    static boolean isTablet() {
+        if (Build.VERSION.SDK_INT >= 17) {
+            DisplayMetrics displayMetrics = Resources.getSystem().getDisplayMetrics();
+            float density = displayMetrics.density;
+            double inch = Math.sqrt(Math.pow(displayMetrics.widthPixels, 2) + Math.pow(displayMetrics.heightPixels, 2)) / (160 * density);
+            return inch >= 8.0d;
+        }
+        return isApproximateTablet();
+    }
+
+    private static boolean isApproximateTablet() {
+        DisplayMetrics displayMetrics = Resources.getSystem().getDisplayMetrics();
+        int width = displayMetrics.widthPixels;
+        int height = displayMetrics.heightPixels;
+        float density = displayMetrics.density;
+        double inch = Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2)) / (160 * density);
+        return inch >= 8.0d;
+    }
+
+    static void initPubNativeSDK(String appToken, Activity activity) {
+        PNLite.initialize(appToken, activity.getApplication());
+        PNLite.setTestMode(YumiDebug.isDebugMode());
+
+    }
+
+    static void updateGDPRStatus() {
+        if (YumiSettings.getGDPRStatus() == YumiGDPRStatus.UNKNOWN) {
+            return;
+        }
+        boolean isConsent = YumiSettings.getGDPRStatus() == YumiGDPRStatus.PERSONALIZED;
+        //https://developers.pubnative.net/docs/hybid-android-sdk-gdpr-configuration
+        if (isConsent) {
+            PNLite.getUserDataManager().grantConsent();
+        } else {
+            PNLite.getUserDataManager().denyConsent();
+        }
+    }
+
+}
---
+++ pubnative/src/main/java/com/yumi/android/sdk/ads/adapter/pubnative/PubnativeInterstitialAdapter.java
@@ -0,0 +1,116 @@
+package com.yumi.android.sdk.ads.adapter.pubnative;
+
+import android.app.Activity;
+
+import com.yumi.android.sdk.ads.beans.YumiProviderBean;
+import com.yumi.android.sdk.ads.publish.YumiDebug;
+import com.yumi.android.sdk.ads.publish.adapter.YumiCustomerInterstitialAdapter;
+import com.yumi.android.sdk.ads.utils.ZplayDebug;
+
+import net.pubnative.lite.sdk.PNLite;
+import net.pubnative.lite.sdk.interstitial.PNInterstitialAd;
+
+import static com.yumi.android.sdk.ads.adapter.pubnative.PubNativeUtil.initPubNativeSDK;
+import static com.yumi.android.sdk.ads.adapter.pubnative.PubNativeUtil.recodeError;
+import static com.yumi.android.sdk.ads.adapter.pubnative.PubNativeUtil.updateGDPRStatus;
+
+public class PubnativeInterstitialAdapter extends YumiCustomerInterstitialAdapter {
+    private String TAG = "PubnativeInterstitialAdapter";
+    private PNInterstitialAd mInterstitial;
+    private PNInterstitialAd.Listener mInterstitialListener;
+
+    protected PubnativeInterstitialAdapter(Activity activity, YumiProviderBean provider) {
+        super(activity, provider);
+    }
+
+    @Override
+    protected void onPrepareInterstitial() {
+        ZplayDebug.i(TAG, "pubnative request new interstitial key2:" + getProvider().getKey2(), onoff);
+        if (mInterstitial == null) {
+            mInterstitial = new PNInterstitialAd(getActivity(), getProvider().getKey2(), mInterstitialListener);
+        }
+
+        mInterstitial.load();
+    }
+
+    @Override
+    protected void onShowInterstitialLayer(Activity activity) {
+        if (mInterstitial != null) {
+            mInterstitial.show();
+        }
+    }
+
+    @Override
+    protected boolean isInterstitialLayerReady() {
+        if (mInterstitial != null) {
+            return mInterstitial.isReady();
+        }
+        return false;
+    }
+
+    @Override
+    protected void init() {
+        ZplayDebug.i(TAG, "pubnative interstitial init key1:" + getProvider().getKey1(), onoff);
+        initPubNativeSDK(getProvider().getKey1(), getActivity());
+        updateGDPRStatus();
+        createInterstitialListener();
+    }
+
+    private void createInterstitialListener() {
+        mInterstitialListener = new PNInterstitialAd.Listener() {
+
+            @Override
+            public void onInterstitialLoaded() {
+                ZplayDebug.i(TAG, "pubnative interstitial loaded", onoff);
+                layerPrepared();
+            }
+
+            @Override
+            public void onInterstitialLoadFailed(Throwable throwable) {
+                ZplayDebug.i(TAG, "pubnative interstitial loadFailed" + throwable, onoff);
+                layerPreparedFailed(recodeError(throwable.toString()));
+            }
+
+            @Override
+            public void onInterstitialImpression() {
+                ZplayDebug.i(TAG, "pubnative interstitial impression", onoff);
+                layerStartPlaying();
+                layerExposure();
+            }
+
+            @Override
+            public void onInterstitialDismissed() {
+                ZplayDebug.i(TAG, "pubnative interstitial dismissed", onoff);
+                layerClosed();
+            }
+
+            @Override
+            public void onInterstitialClick() {
+                ZplayDebug.i(TAG, "pubnative interstitial click", onoff);
+                layerClicked(-999f, -999f);
+            }
+        };
+    }
+
+    @Override
+    public void onActivityPause() {
+
+    }
+
+    @Override
+    public void onActivityResume() {
+
+    }
+
+    @Override
+    public boolean onActivityBackPressed() {
+        return false;
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mInterstitial != null) {
+            mInterstitial.destroy();
+        }
+    }
+}
