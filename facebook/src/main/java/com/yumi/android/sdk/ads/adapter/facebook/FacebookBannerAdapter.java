f9c8744a5155538aab06514432045f9e80d7b1a1
“yangfengbang” <yangfengbang@zplay.cn>
2019-09-10 11:07:57 +0800

    add facebook init method

--- facebook/src/main/java/com/yumi/android/sdk/ads/adapter/facebook/FacebookBannerAdapter.java
+++ facebook/src/main/java/com/yumi/android/sdk/ads/adapter/facebook/FacebookBannerAdapter.java
@@ -12,6 +12,7 @@ import com.yumi.android.sdk.ads.publish.adapter.YumiCustomerBannerAdapter;
 import com.yumi.android.sdk.ads.utils.ZplayDebug;
 
 import static com.facebook.ads.AdError.NO_FILL;
+import static com.yumi.android.sdk.ads.adapter.facebook.FacebookUtil.initSDK;
 import static com.yumi.android.sdk.ads.adapter.facebook.FacebookUtil.sdkVersion;
 import static com.yumi.android.sdk.ads.publish.enumbean.AdSize.BANNER_SIZE_SMART;
 
@@ -60,6 +61,7 @@ public class FacebookBannerAdapter extends YumiCustomerBannerAdapter {
     @Override
     protected void init() {
         ZplayDebug.i(TAG, "placementID : " + getProvider().getKey1(), onoff);
+        initSDK(getContext());
         createBannerListener();
     }
 
