0ed7eff7d90f9bb75dbb2bfea5dcfebc45259fe2
zmer007 <developer.lgd@gmail.com>
2019-10-28 16:58:50 +0800

    remove useless comments and field

--- ironsource/src/main/java/com/yumi/android/sdk/ads/adapter/ironsource/IronsourceInterstitialAdapter.java
+++ ironsource/src/main/java/com/yumi/android/sdk/ads/adapter/ironsource/IronsourceInterstitialAdapter.java
@@ -22,7 +22,6 @@ import static com.yumi.android.sdk.ads.publish.enumbean.LayerErrorCode.ERROR_FAI
 public class IronsourceInterstitialAdapter extends YumiCustomerInterstitialAdapter {
 
     private static final String TAG = "IronsourceInterstitialAdapter";
-    private ISDemandOnlyInterstitialListener adListener;
 
     protected IronsourceInterstitialAdapter(Activity activity, YumiProviderBean yumiProviderBean) {
         super(activity, yumiProviderBean);
@@ -55,88 +54,62 @@ public class IronsourceInterstitialAdapter extends YumiCustomerInterstitialAdapt
     @Override
     protected void init() {
         final String appKey = getProvider().getKey1();
-        ZplayDebug.d(TAG, "init: " + appKey);
-        createMediaListener();
+        final String instanceId = getProvider().getKey2();
+        ZplayDebug.d(TAG, "init: " + appKey + ", instanceId: " + instanceId);
+        IronsourceListenerHandler.setMyIronsourceInterstitialListener(instanceId, new ISDemandOnlyInterstitialListener() {
+
+            @Override
+            public void onInterstitialAdReady(String instanceId) {
+                ZplayDebug.d(TAG, "onInterstitialAdReady: " + instanceId);
+                if (instanceId.equals(getProvider().getKey2())) {
+                    layerPrepared();
+                }
+            }
+
+            @Override
+            public void onInterstitialAdLoadFailed(String instanceId, IronSourceError error) {
+                ZplayDebug.d(TAG, "onInterstitialAdLoadFailed: " + instanceId + ", error: " + error);
+                if (instanceId.equals(getProvider().getKey2())) {
+                    layerPreparedFailed(generateLayerErrorCode(error));
+                }
+            }
+
+            @Override
+            public void onInterstitialAdOpened(String instanceId) {
+                ZplayDebug.d(TAG, "onInterstitialAdOpened: " + instanceId);
+                if (instanceId.equals(getProvider().getKey2())) {
+                    layerExposure();
+                    layerStartPlaying();
+                }
+            }
+
+            @Override
+            public void onInterstitialAdClosed(String instanceId) {
+                ZplayDebug.d(TAG, "onInterstitialAdClosed: " + instanceId);
+                if (instanceId.equals(getProvider().getKey2())) {
+                    layerClosed();
+                }
+            }
+
+            @Override
+            public void onInterstitialAdShowFailed(String instanceId, IronSourceError error) {
+                ZplayDebug.d(TAG, "onInterstitialAdShowFailed: " + instanceId + ", error: " + error);
+                if (TextUtils.equals(instanceId, getProvider().getKey2())) {
+                    AdError adError = new AdError(ERROR_FAILED_TO_SHOW);
+                    adError.setErrorMessage("IronSource errorMsg: " + error);
+                    layerExposureFailed(adError);
+                }
+            }
+
+            @Override
+            public void onInterstitialAdClicked(String instanceId) {
+                ZplayDebug.d(TAG, "onInterstitialAdClicked: " + instanceId);
+                if (instanceId.equals(getProvider().getKey2())) {
+                    layerClicked(-99f, -99f);
+                }
+            }
+        });
         IronsourceListenerHandler.initIronsourceInterstitialListener(getActivity(), appKey);
-    }
-
-
-    private void createMediaListener() {
-        if (adListener == null) {
-            adListener = new ISDemandOnlyInterstitialListener() {
-
-                /**
-                 * Invoked when Interstitial Ad is ready to be shown after load function was called.
-                 */
-                @Override
-                public void onInterstitialAdReady(String instanceId) {
-                    ZplayDebug.d(TAG, "onInterstitialAdReady: " + instanceId);
-                    if (instanceId.equals(getProvider().getKey2())) {
-                        layerPrepared();
-                    }
-                }
-
-                /**
-                 * invoked when there is no Interstitial Ad available after calling load function.
-                 */
-                @Override
-                public void onInterstitialAdLoadFailed(String instanceId, IronSourceError error) {
-                    ZplayDebug.d(TAG, "onInterstitialAdLoadFailed: " + instanceId + ", error: " + error);
-                    if (instanceId.equals(getProvider().getKey2())) {
-                        layerPreparedFailed(generateLayerErrorCode(error));
-                    }
-                }
-
-                /**
-                 * Invoked when the Interstitial Ad Unit is opened
-                 */
-                @Override
-                public void onInterstitialAdOpened(String instanceId) {
-                    ZplayDebug.d(TAG, "onInterstitialAdOpened: " + instanceId);
-                    if (instanceId.equals(getProvider().getKey2())) {
-                        layerExposure();
-                        layerStartPlaying();
-                    }
-                }
-
-                /*
-                 * Invoked when the ad is closed and the user is about to return to the application.
-                 */
-                @Override
-                public void onInterstitialAdClosed(String instanceId) {
-                    ZplayDebug.d(TAG, "onInterstitialAdClosed: " + instanceId);
-                    if (instanceId.equals(getProvider().getKey2())) {
-                        layerClosed();
-                    }
-                }
-
-                /**
-                 * Invoked when Interstitial ad failed to show.
-                 * // @param error - An object which represents the reason of showInterstitial failure.
-                 */
-                @Override
-                public void onInterstitialAdShowFailed(String instanceId, IronSourceError error) {
-                    ZplayDebug.d(TAG, "onInterstitialAdShowFailed: " + instanceId + ", error: " + error);
-                    if (TextUtils.equals(instanceId, getProvider().getKey2())) {
-                        AdError adError = new AdError(ERROR_FAILED_TO_SHOW);
-                        adError.setErrorMessage("IronSource errorMsg: " + error);
-                        layerExposureFailed(adError);
-                    }
-                }
-
-                /*
-                 * Invoked when the end user clicked on the interstitial ad.
-                 */
-                @Override
-                public void onInterstitialAdClicked(String instanceId) {
-                    ZplayDebug.d(TAG, "onInterstitialAdClicked: " + instanceId);
-                    if (instanceId.equals(getProvider().getKey2())) {
-                        layerClicked(-99f, -99f);
-                    }
-                }
-            };
-            IronsourceListenerHandler.setMyIronsourceInterstitialListener(getProvider().getKey2(), adListener);
-        }
     }
 
     @Override
