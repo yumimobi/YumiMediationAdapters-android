dc7b4fb3c7a4b308cc8a75501406e7bc5df5d14e
zmer007 <developer.lgd@gmail.com>
2019-10-29 16:33:04 +0800

    fine tune unity onPrepared callback

--- unity/src/main/java/com/yumi/android/sdk/ads/adapter/unity/UnityUtil.java
+++ unity/src/main/java/com/yumi/android/sdk/ads/adapter/unity/UnityUtil.java
@@ -9,15 +9,25 @@ import com.yumi.android.sdk.ads.publish.YumiSettings;
 import com.yumi.android.sdk.ads.publish.enumbean.LayerErrorCode;
 import com.yumi.android.sdk.ads.publish.enumbean.YumiGDPRStatus;
 
+import static com.yumi.android.sdk.ads.publish.enumbean.LayerErrorCode.ERROR_INTERNAL;
+import static com.yumi.android.sdk.ads.publish.enumbean.LayerErrorCode.ERROR_NO_FILL;
+
 /**
  * Description:
  * <p>
  * Created by lgd on 2019/1/23.
  */
-public class UnityUtil {
+class UnityUtil {
     static AdError generateLayerErrorCode(UnityAds.UnityAdsError unityAdsError, String message) {
-        AdError result = new AdError(LayerErrorCode.ERROR_INTERNAL);
+        AdError result = new AdError(ERROR_INTERNAL);
         result.setErrorMessage("Unity errorMsg: " + unityAdsError + ", " + message);
+        return result;
+    }
+
+    static AdError generateLayerErrorCode(UnityAds.PlacementState unityState, String message) {
+        LayerErrorCode code = unityState == UnityAds.PlacementState.NO_FILL ? ERROR_NO_FILL : ERROR_INTERNAL;
+        AdError result = new AdError(code);
+        result.setErrorMessage("Unity errorMsg: " + unityState + ", " + message);
         return result;
     }
 
